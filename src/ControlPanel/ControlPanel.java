a01a4a48983295d2e29d283834c709d3d2174d2b
Lucas Maldonacho <31268463+Maldonacho@users.noreply.github.com>
2020-03-19 13:34:15 +1000

    Created placeholder ControlPanel UI

--- src/ControlPanel/ControlPanel.java
+++ src/ControlPanel/ControlPanel.java
@@ -1,7 +1,37 @@
 package ControlPanel;
 
+import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * ControlPanel remotely controls the Server. This is the main class of the
+ * ControlPanel app.
+ */
 public class ControlPanel {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		setupUI();
+	}
+
+	private static void setupUI() {
+		// Create and setup main window
+		JFrame mainWindow = new JFrame("Billboard Control Panel");
+		mainWindow.setSize(400, 250); // Size of the main window
+		mainWindow.setLayout(new FlowLayout()); // Arrange widgets centered left to right, top to bottom
+		mainWindow.setVisible(true);
+
+		// Create a bit of text
+		mainWindow.add(new JLabel("Hello World!"));
+
+		// Create an ugly button
+		JButton exitButton = new JButton("Click me");
+		mainWindow.add(exitButton); // Add button to window
+
+		// Do stuff when button is clicked
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Button pressed");
+			}
+		});
 	}
 }

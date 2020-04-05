b559861b9a70fe83967821aea3bd809c2c00e4aa
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2020-04-06 03:55:46 +1000

    Added Users form

--- src/ControlPanel/MainWindow.java
+++ src/ControlPanel/MainWindow.java
@@ -1,6 +1,8 @@
 package ControlPanel;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Graphical User Interface for main window form
@@ -14,6 +16,19 @@ public class MainWindow {
 	public JPanel mainWindow;
 	private JTable mainWindowTable;
 
+
+	public MainWindow() {
+		editUsersButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame usersFrame = new JFrame("Users");
+				usersFrame.setContentPane(new User().Users);
+				usersFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				usersFrame.pack();
+				usersFrame.setVisible(true);
+			}
+		});
+	}
 
 	public static void main(String[] args) {
 		JFrame mainFrame = new JFrame("MainWindow");
---
+++ src/ControlPanel/User.java
@@ -0,0 +1,21 @@
+package ControlPanel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class User {
+	public JPanel Users;
+	private JButton btnNewUser;
+	private JList lstNames;
+	private JButton btnSelect;
+
+	public User() {
+		btnNewUser.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+	}
+}

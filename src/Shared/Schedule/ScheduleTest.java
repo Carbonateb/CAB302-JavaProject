215773971fc057f7f757fc6fef7f7fbfafb54200
Lucas Maldonacho <31268463+Maldonacho@users.noreply.github.com>
2020-05-26 21:39:10 +1000

    Update ScheduleTest.java

--- src/Shared/Schedule/ScheduleTest.java
+++ src/Shared/Schedule/ScheduleTest.java
@@ -9,23 +9,5 @@ class ScheduleTest {
     @Test
     void testRun() {
     	Schedule schedule = new Schedule();
-
-    	// Create an event that goes from 0 to 5
-    	assertTrue(schedule.addEvent(new Event(0, 5, 0, 0)));
-
-    	// Create an event from 5 to 10, this should work too
-    	assertTrue(schedule.addEvent(new Event(5, 10, 0, 0)));
-
-    	// This event (4 to 6) should NOT be valid, as it overlaps with the one created earlier (0 to 5)
-		assertFalse(schedule.canFit(4, 6));
-
-
-		// Removes the first event we added (the 0 to 5 one)
-		assertTrue(schedule.removeEvent(0));
-
-		// Verify it got removed by adding another event that would otherwise conflict with it if it were still there
-		assertTrue(schedule.canFit(1, 4));
-
-
     }
 }

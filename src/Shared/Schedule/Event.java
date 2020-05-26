11f002578d592f71d25129b91ff75da43e0b9a6f
Lucas Maldonacho <31268463+Maldonacho@users.noreply.github.com>
2020-05-26 14:16:44 +1000

    Added utility functions to Event

--- src/Shared/Schedule/Event.java
+++ src/Shared/Schedule/Event.java
@@ -9,8 +9,49 @@ package Shared.Schedule;
  * @author Lucas Maldonado n10534342
  */
 public class Event {
-	long startTime;
-	long endTime;
-	int billboardID; // Reference to the billboard to display
-	int creatorID; // The user that created this event
+
+	// Times are measured in milliseconds since unix epoch
+	public long startTime;
+	public long endTime;
+
+	public int billboardID; // Reference to the billboard to display
+	public int creatorID; // The user that created this event
+
+
+	// Constructor
+	public Event(long inStartTime, long inEndTime, int inBillboardID, int inCreatorID) {
+		startTime = inStartTime;
+		endTime = inEndTime;
+		billboardID = inBillboardID;
+		creatorID = inCreatorID;
+	}
+
+
+	/** Gets how long this billboard will run for */
+	public long getDuration() {
+		return endTime - startTime;
+	}
+
+
+	/** Sets endTime so that Duration will be this amount */
+	public void setEndTime(long duration) {
+		endTime = startTime + duration;
+	}
+
+
+	/** Check if this event conflicts with another given event */
+	public boolean overlapsWith(Event other) {
+		// If this event is earlier
+		if (startTime < other.startTime) {
+			return endTime > other.startTime;
+		}
+
+		// If this event is later
+		if (startTime > other.startTime) {
+			return other.endTime > startTime;
+		}
+
+		// If we're here it means both startTimes are identical, so there is an overlap
+		return true;
+	}
 }

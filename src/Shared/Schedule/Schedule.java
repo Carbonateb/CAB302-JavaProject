a1aed38da356ab557b16744616c5f23866b3e4a5
Lucas Maldonacho <31268463+Maldonacho@users.noreply.github.com>
2020-05-26 20:05:28 +1000

    Schedule now supports basic one-time events

--- src/Shared/Schedule/Schedule.java
+++ src/Shared/Schedule/Schedule.java
@@ -1,5 +1,7 @@
 package Shared.Schedule;
 
+import java.util.ArrayList;
+
 /**
  * The Schedule manages a collection of Events. You can add billboards to the Schedule by adding Events.
  * Complex behaviour like looping events is also supported here.
@@ -7,4 +9,69 @@ package Shared.Schedule;
  * @author Lucas Maldonado n10534342
  */
 public class Schedule {
+
+	/** The main storage container for all of the events */
+	private ArrayList<Event> events = new ArrayList<Event>();
+
+
+	// Constructor
+	public Schedule() {
+	}
+
+
+	/** Adds a event to the schedule */
+	public boolean addEvent(Event newEvent) {
+		if (canFit(newEvent)) {
+			events.add(newEvent);
+			return true;
+		}
+		return false;
+	}
+
+
+	/** Removes an event from the schedule. Returns true if successful */
+	public boolean removeEvent(long startTime) {
+		for (int i = 0; i < events.size(); ++i) {
+			if (events.get(i).startTime == startTime) {
+				events.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	/** Removes an event from the schedule, given an entire event. Returns true if successful */
+	public boolean removeEvent(Event event) {
+		return removeEvent(event.startTime);
+	}
+
+
+	/** Checks if a new event can fit in the schedule without any conflicts */
+	public boolean canFit(long testStartTime, long testEndTime) {
+		for (Event e : events) {
+			if (e.overlapsWith(testStartTime, testEndTime)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	/** Checks if a new event can fit in the schedule without any conflicts */
+	public boolean canFit(Event contender) {
+		return canFit(contender.startTime, contender.endTime);
+	}
+
+
+	/** Gets the billboard that should be displaying now */
+	public Event getCurrentEvent() {
+		return new Event(0, 0, 0, 0);
+	}
+
+
+	private long getTime() {
+		return System.currentTimeMillis();
+	}
+
 }

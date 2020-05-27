e284d8243f6252f93b18cd41238ed5ed13543065
Connor McHugh <connor.mchugh5@gmail.com>
2020-05-28 07:54:44 +1000

    Update Viewer.java

--- src/Viewer/Viewer.java
+++ src/Viewer/Viewer.java
@@ -1,20 +1,35 @@
 package Viewer;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
+import Shared.Display.IMGHandler;
+import Shared.Display.XMLHandler;
+import org.xml.sax.SAXException;
 
 /**
  * The viewer is the dummy client that displays the billboard.
  *
- * @author Lucas Maldonado - n10534342
- * @author Connor McHugh - n10522662
+ * @author Lucas Maldonado N10534342
  */
 public class Viewer {
 	private JPanel mainPanel;
-	private JTextPane testTextPane;
+	private JTextPane message;
+	private JTextPane information;
+	private JTextPane image;
 
 	/**
 	 * How often the Viewer should get an update from the server, in seconds.
@@ -30,6 +45,123 @@ public class Viewer {
 		CreateUI();
 		CreateUpdateTimer();
 		System.out.println("Viewer started successfully");
+
+		message.addMouseListener(new MouseAdapter() {
+			/**
+			 * {@inheritDoc}
+			 *
+			 * @param e
+			 */
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == 1)
+					System.exit(0);
+			}
+		});
+		image.addMouseListener(new MouseAdapter() {
+			/**
+			 * {@inheritDoc}
+			 *
+			 * @param e
+			 */
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == 1)
+					System.exit(0);
+			}
+		});
+		information.addMouseListener(new MouseAdapter() {
+			/**
+			 * {@inheritDoc}
+			 *
+			 * @param e
+			 */
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == 1)
+					System.exit(0);
+			}
+		});
+	}
+
+
+	private void populateViewer() {
+//		String xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><billboard background=\"#6800C0\"><message colour=\"#FF9E3F\">All custom colours</message><information colour=\"#3FFFC7\">All custom colours</information><picture url=\"https://cloudstor.aarnet.edu.au/plus/s/X79GyWIbLEWG4Us/download\" /></billboard>\n";
+		String xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><billboard background=\"#6800C0\"><message colour=\"#FF9E3F\">All custom colours</message><information colour=\"#3FFFC7\">All custom colours</information><picture data=\"iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAApElEQVR42u3RAQ0AAAjDMO5fNCCDkC5z0HTVrisFCBABASIgQAQEiIAAAQJEQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAQECBAgAgJEQIAIyPcGFY7HnV2aPXoAAAAASUVORK5CYII=\" />\n</billboard>\n";
+
+
+		try {
+			// Set message text and color
+			Color messageColor;
+			String messageText = XMLHandler.xmlReader(false, xmlData, "message", null);
+			if (messageText != null) {
+				message.setText(messageText);
+				try {
+					messageColor = Color.decode(Objects.requireNonNull(XMLHandler.xmlReader(false, xmlData, "message", "colour")));
+				} catch (NullPointerException r) {
+					messageColor = Color.black;
+				}
+				message.setForeground(messageColor);
+				StyledDocument test = message.getStyledDocument();
+				SimpleAttributeSet test2 = new SimpleAttributeSet();
+				StyleConstants.setAlignment(test2, StyleConstants.ALIGN_CENTER);
+				test.setParagraphAttributes(0, test.getLength()-1, test2, false);
+			}
+
+			// Set information text and color
+			Color informationColor;
+			String informationText = XMLHandler.xmlReader(false, xmlData, "information", null);
+			if (informationText != null) {
+				information.setText(informationText);
+				try {
+					informationColor = Color.decode(Objects.requireNonNull(XMLHandler.xmlReader(false, xmlData, "information", "colour")));
+				} catch (NullPointerException r) {
+					informationColor = Color.black;
+				}
+				information.setForeground(informationColor);
+				StyledDocument test = information.getStyledDocument();
+				SimpleAttributeSet test2 = new SimpleAttributeSet();
+				StyleConstants.setAlignment(test2, StyleConstants.ALIGN_CENTER);
+				test.setParagraphAttributes(0, test.getLength()-1, test2, false);
+			}
+
+			// Set background color
+			Color backgroundColor;
+			try {
+				backgroundColor = Color.decode(Objects.requireNonNull(XMLHandler.xmlReader(false, xmlData, "billboard", "background")));
+			} catch (NullPointerException r) {
+				backgroundColor = Color.white;
+			}
+			mainPanel.setBackground(backgroundColor);
+			message.setBackground(backgroundColor);
+			information.setBackground(backgroundColor);
+			image.setBackground(backgroundColor);
+
+			// Set image
+			String imageString;
+			BufferedImage billboardImage = null;
+			try {
+				imageString = XMLHandler.xmlReader(false, xmlData, "picture", "data");
+				billboardImage = IMGHandler.imageDecoder(imageString);
+			} catch (NullPointerException ignored) { }
+
+			try {
+				imageString = XMLHandler.xmlReader(false, xmlData, "picture", "url");
+				assert imageString != null;
+				billboardImage = ImageIO.read(new URL(imageString));
+			} catch (NullPointerException | MalformedURLException ignored) { }
+
+			assert billboardImage != null;
+			ImageIcon displayedBillboardImage = new ImageIcon(billboardImage);
+			image.insertIcon(displayedBillboardImage);
+			StyledDocument test = image.getStyledDocument();
+			SimpleAttributeSet test2 = new SimpleAttributeSet();
+			StyleConstants.setAlignment(test2, StyleConstants.ALIGN_CENTER);
+			test.setParagraphAttributes(0, test.getLength()-1, test2, false);
+
+		} catch (ParserConfigurationException | IOException | SAXException | NullPointerException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 
@@ -74,8 +206,7 @@ public class Viewer {
 		});
 		mainPanel.getActionMap().put("exitProgram", exitProgram);
 
-		testTextPane.setBackground(new Color(0,0,0,0));
-		mainPanel.setBackground(Color.BLACK);
+		populateViewer();
 	}
 
 

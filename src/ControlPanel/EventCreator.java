57497839e23f2391a6c4bbb5b8de04586a6716d6
Lucas Maldonacho <31268463+Maldonacho@users.noreply.github.com>
2020-05-30 03:03:01 +1000

    Converted user input to epoch seconds

--- src/ControlPanel/EventCreator.java
+++ src/ControlPanel/EventCreator.java
@@ -3,8 +3,14 @@ package ControlPanel;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class EventCreator extends JFrame {
 	private JPanel main_Panel;
@@ -19,6 +25,9 @@ public class EventCreator extends JFrame {
 	private JSpinner loopDays_Spinner;
 	private JSpinner loopHours_spinner;
 	private JSpinner loopMinutes_Spinner;
+
+	private SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a");
+	private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d/M/yyyy", Locale.ENGLISH);
 
 	/**
 	 * Allows the user to schedule a billboard. Has controls for setting a start date & time, duration, and looping
@@ -38,14 +47,27 @@ public class EventCreator extends JFrame {
 		setVisible(true);
 
 
+		// Prefill date & time boxes with current time
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		startTime_TextField.setText(timeFormatter.format(new Date()));
+		startDate_TextField.setText(currentDateTime.format(dateFormatter));
 
 
-		startTime_TextField.setText(new SimpleDateFormat("h:mm a").format(new Date()));
-		startDate_TextField.setText(new SimpleDateFormat("dd/mm/yyyy").format(new Date()));
 
 		ok_Button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				// The user specified start time, in unix epoch milliseconds
+				long startTime = 0;
+				try {
+					// 86400000 is number of seconds in day (86400) * milliseconds in second (1000)
+					// This converts epoch days to epoch milliseconds
+					startTime += 86400000 * LocalDate.parse(startDate_TextField.getText(), dateFormatter).toEpochDay();
+					// Add on the hours and minutes, and we're good to go
+					startTime += timeFormatter.parse(startTime_TextField.getText()).getTime();
+				} catch (DateTimeException | ParseException ex) {
+					System.out.println(ex.getMessage());
+				}
 
 
 
@@ -54,6 +76,7 @@ public class EventCreator extends JFrame {
 		cancel_Button.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				dispose();
 			}
 		});
 	}

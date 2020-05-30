cb02f0aeb19e1946c1f0398d83df38985bf3e966
Lucas Maldonacho <31268463+Maldonacho@users.noreply.github.com>
2020-05-30 17:31:13 +1000

    Finished Tabbed UI Prototype

--- src/ControlPanel/MainWindow.java
+++ src/ControlPanel/MainWindow.java
@@ -1,24 +1,82 @@
 package ControlPanel;
 
+import Server.Endpoints.EndpointType;
+import Shared.Network.Response;
+import Shared.Schedule.Event;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Graphical User Interface for main window form
  */
 
 public class MainWindow {
-	private JButton createButton;
-	private JButton editButton;
-	private JButton scheduleButton;
+	private JButton newBillboard_Button;
 	private JButton editUsersButton;
 	public JPanel mainWindow;
+	private JTable schedule_Table;
+	private JButton newEvent_Button;
 	private JTable mainWindowTable;
 
 
-
 	public MainWindow() {
+
+		/**
+		 * Queries the server for billboard information and displays in table
+		 *
+		 * @author Callum McNeilage - n10482652
+		 */
+		//Queries server to return billboard schedule
+		ArrayList<Event> list = null;
+		try {
+			Response response = ControlPanel.get().requestSender.SendData(EndpointType.getEvents, null);
+
+			// Check if the server replied with an error
+			if (response.getStatus().equals("error")) {
+				if (response.getData().equals("Invalid token")) {
+					System.out.println("Your session token is invalid (probably expired), try logging in again");
+				}
+				else {
+					System.out.println("Server replied with an error: " + response.getData().toString());
+				}
+				return;
+			}
+
+			if (response.getData() instanceof ArrayList) {
+				list = (ArrayList<Event>) response.getData();
+			} else if(response.getData().toString().equals("")){
+				System.out.println("No billboards to show");
+			}
+			else {
+				System.out.printf("Server replied with unexpected data: '%s'\n", response.getData().toString());
+				return;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("There was an issue when retrieving billboards");
+		}
+
+		//Data to be displayed in the JTable
+		DefaultTableModel model = (DefaultTableModel) schedule_Table.getModel();
+		model.addColumn("BillboardEditor");
+		model.addColumn("Start Time");
+
+		for (int i = 0; i < list.size(); i++) {
+			Object[] row = new Object[2];
+			row[0] = list.get(i).billboardName;
+			row[1] = DateFormat(list.get(i).startTime);
+
+			model.addRow(row);
+		}
+
+
+
 		/**
 		 * Opens the User form when editUsersButton is clicked
 		 *
@@ -31,20 +89,10 @@ public class MainWindow {
 			}
 		});
 
-		/**
-		 * Opens the Schedule form when Schedule button if clicked
-		 *
-		 * @author Callum McNeilage
-		 */
-		scheduleButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Schedule.main(null);
-			}
-		});
-		createButton.addActionListener(new ActionListener() {
+
+		newBillboard_Button.addActionListener(new ActionListener() {
 			/**
-			 * Opens New BillboardEditor form when createButton is pressed
+			 * Opens New BillboardEditor form when newBillboard_Button is pressed
 			 *
 			 * @author Connor McHugh - n10522662
 			 * @param e the event to be processed
@@ -55,14 +103,15 @@ public class MainWindow {
 			}
 		});
 
-		editButton.addActionListener(new ActionListener() {
+		/*
+		newBillboard_Button.addActionListener(new ActionListener() {
 
 			/**
 			 * Opens pre-populated billboard form when editButton is pressed
 			 *
 			 * @author Callum McNeilage - n10482652
 			 * @param e the event to be processed
-			 */
+			 /
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				String testXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><billboard background=\"#6800C0\"><message colour=\"#FF9E3F\">All custom colours</message><information colour=\"#3FFFC7\">All custom colours</information><picture url=\"https://cloudstor.aarnet.edu.au/plus/s/X79GyWIbLEWG4Us/download\" /></billboard>\n";
@@ -77,5 +126,25 @@ public class MainWindow {
 				}
 			}
 		});
+		*/
+		newEvent_Button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new EventEditor();
+			}
+		});
+	}
+
+	/**
+	 * Takes a date formatted as a long and converts it to Simple Date Format to display in schedule
+	 *
+	 * @author Callum McNeilage - n10482652
+	 * @param unix date formatted as a long
+	 * @return Date formatted as yyyy-MM-dd HH:mm:ss z
+	 */
+	private String DateFormat(long unix) {
+		Date date = new Date(unix*1000L);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+		return sdf.format(date);
 	}
 }
--- src/ControlPanel/Schedule.java
+++ src/ControlPanel/Schedule.java
@@ -1,16 +1,9 @@
 package ControlPanel;
 
-import Server.Endpoints.EndpointType;
-import Shared.Network.Response;
-import Shared.Schedule.Event;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 
 public class Schedule {
 	private JPanel Schedule;
@@ -52,66 +45,7 @@ public class Schedule {
 			}
 		});
 
-		/**
-		 * Queries the server for billboard information and displays in table
-		 *
-		 * @author Callum McNeilage - n10482652
-		 */
-		//Queries server to return billboard schedule
-		ArrayList<Event> list = null;
-		try {
-			Response response = ControlPanel.get().requestSender.SendData(EndpointType.getEvents, null);
 
-			// Check if the server replied with an error
-			if (response.getStatus().equals("error")) {
-				if (response.getData().equals("Invalid token")) {
-					System.out.println("Your session token is invalid (probably expired), try logging in again");
-				}
-				else {
-					System.out.println("Server replied with an error: " + response.getData().toString());
-				}
-				return;
-			}
-
-			if (response.getData() instanceof ArrayList) {
-				list = (ArrayList<Event>) response.getData();
-			} else if(response.getData().toString().equals("")){
-				System.out.println("No billboards to show");
-			}
-			else {
-				System.out.printf("Server replied with unexpected data: '%s'\n", response.getData().toString());
-				return;
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-			System.out.println("There was an issue when retrieving billboards");
-		}
-
-		//Data to be displayed in the JTable
-		DefaultTableModel model = (DefaultTableModel) table1.getModel();
-		model.addColumn("BillboardEditor");
-		model.addColumn("Start Time");
-
-		for (int i = 0; i < list.size(); i++) {
-			Object[] row = new Object[2];
-			row[0] = list.get(i).billboardName;
-			row[1] = DateFormat(list.get(i).startTime);
-
-			model.addRow(row);
-		}
-	}
-
-	/**
-	 * Takes a date formatted as a long and converts it to Simple Date Format to display in schedule
-	 *
-	 * @author Callum McNeilage - n10482652
-	 * @param unix date formatted as a long
-	 * @return Date formatted as yyyy-MM-dd HH:mm:ss z
-	 */
-	private String DateFormat(long unix) {
-		Date date = new Date(unix*1000L);
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-		return sdf.format(date);
 	}
 
 	/**

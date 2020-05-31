fa81f6531389bec9369fd9cd49222af37c5211ee
Colby <31269003+Colbyjdx@users.noreply.github.com>
2020-05-31 13:37:50 +1000

    DeleteBillboard server side permissions

--- src/Server/Endpoints/DeleteBillboard.java
+++ src/Server/Endpoints/DeleteBillboard.java
@@ -1,7 +1,12 @@
 package Server.Endpoints;
 
+import Shared.Billboard;
 import Shared.Network.Request;
+import Shared.Network.Response;
 import Shared.Permissions.Perm;
+import Shared.Permissions.Permissions;
+
+import java.io.IOException;
 
 /***
  * end point for deleting a user from the database
@@ -10,8 +15,6 @@ public class DeleteBillboard extends Endpoint {
 	public DeleteBillboard(){
 		// This is the enum value bound to this endpoint
 		associatedEndpoint = EndpointType.deleteBillboard;
-
-		requiredPermission = Perm.EDIT_ALL_BILLBOARDS;
 	}
 
 	/***
@@ -20,7 +23,44 @@ public class DeleteBillboard extends Endpoint {
 	 * @return request status
 	 */
 	public Object executeEndpoint(Request input) {
-		return server.db.rmBillboard((String) input.getData());
+//		return server.db.rmBillboard((String) input.getData());
+		if (!server.db.checkBillboardExists((String) input.getData())) {
+			return false;
+		}
+		Billboard billboard = null;
+		try {
+			billboard = server.db.requestBillboard((String) input.getData());
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+			return false;
+		}
+
+		if (billboard.author.equals(input.getToken().getUser())) {
+			// Deleting their own billboard
+			try {
+				// TODO: check if billboard is scheduled (and deny removal)
+				server.db.rmBillboard((String) input.getData());
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+		} else {
+			Permissions permissions = new Permissions(server.db.getUserDetails(input.getToken().getUser()).getPermissions());
+			// Editing another billboard
+			if (permissions.hasPermission(Perm.EDIT_ALL_BILLBOARDS)) {
+				try {
+					// TODO: remove all scheduled events using this billboard
+					server.db.rmBillboard((String) input.getData());
+					return true;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return false;
+				}
+			} else {
+				return new Response("error", "Permission denied", null);
+			}
+		}
 	}
 
 }
